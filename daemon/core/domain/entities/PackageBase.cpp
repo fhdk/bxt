ec623d44c03eb5952b227badfad5d1dcb8372982
Artem Grinev <agrinev@manjaro.org>
2022-07-11 04:29:42 +0400

    [daemon] Add missing SPDX headers

--- daemon/core/domain/entities/PackageBase.cpp
+++ daemon/core/domain/entities/PackageBase.cpp
@@ -1,3 +1,9 @@
+/* === This file is part of bxt ===
+ *
+ *   SPDX-FileCopyrightText: 2022 Artem Grinev <agrinev@manjaro.org>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
 #include "PackageBase.h"
 
 #include <boost/algorithm/string/classification.hpp>
--- daemon/core/domain/entities/PackageBase.h
+++ daemon/core/domain/entities/PackageBase.h
@@ -1,3 +1,9 @@
+/* === This file is part of bxt ===
+ *
+ *   SPDX-FileCopyrightText: 2022 Artem Grinev <agrinev@manjaro.org>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
 #pragma once
 
 #include "core/domain/entities/AggregateRoot.h"
