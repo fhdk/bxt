4e15f15950278733632b79c2c9aecfab80f8651f
Artem Grinev <agrinev@manjaro.org>
2023-01-10 15:35:51 +0400

    [daemon] Add Box options structure

---
+++ daemon/persistence/alpm/BoxOptions.h
@@ -0,0 +1,23 @@
+/* === This file is part of bxt ===
+ *
+ *   SPDX-FileCopyrightText: 2022 Artem Grinev <agrinev@manjaro.org>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#pragma once
+
+#include "utilities/configuration/Configuration.h"
+namespace bxt::Persistence {
+
+struct BoxOptions : public bxt::Utilities::Configurable {
+    std::string location = "box/";
+
+    virtual void serialize(Utilities::Configuration &config) override {
+        config.set("location", location);
+    }
+    virtual void deserialize(const Utilities::Configuration &config) override {
+        location = config.get<std::string>("location").value_or(location);
+    }
+};
+
+} // namespace bxt::Persistence
