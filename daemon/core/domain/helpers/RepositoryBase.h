6bd81a6ce5a4623953fb528ce2a0553ed4afd318
Artem Grinev <agrinev@manjaro.org>
2023-01-10 15:24:16 +0400

    [daemon] Use std::span for repository

--- daemon/core/domain/helpers/RepositoryBase.h
+++ daemon/core/domain/helpers/RepositoryBase.h
@@ -8,28 +8,28 @@
 
 #include <boost/uuid/uuid.hpp>
 #include <functional>
+#include <span>
 
 namespace bxt::Core::Domain
 {
 
-template<typename TEntity, typename TIterable, typename TId = boost::uuids::uuid>
-struct ReadOnlyRepositoryBase
-{
+template<typename TEntity, typename TId = boost::uuids::uuid>
+struct ReadOnlyRepositoryBase {
     virtual ~ReadOnlyRepositoryBase() = default;
 
     virtual TEntity find_by_id(const TId& id) = 0;
     virtual TEntity find_first(std::function<bool(const TEntity&)>) = 0;
-    virtual TIterable find(std::function<bool(const TEntity&)>) = 0;
+    virtual std::span<TEntity> find(std::function<bool(const TEntity&)>) = 0;
 };
 
-template<typename TEntity, typename TIterable, typename TId = boost::uuids::uuid>
-struct RepositoryBase
-{
+template<typename TEntity, typename TId = boost::uuids::uuid>
+struct RepositoryBase {
     virtual ~RepositoryBase() = default;
 
     virtual TEntity find_by_id(const TId& id) = 0;
     virtual TEntity find_first(std::function<bool(const TEntity&)>) = 0;
-    virtual TIterable find(std::function<bool(const TEntity&)>) = 0;
+    virtual std::span<TEntity>
+        find(const std::function<bool(const TEntity&)>&) = 0;
     virtual void add(const TEntity& entity) = 0;
     virtual void remove(const TEntity& entity) = 0;
 };
