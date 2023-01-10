791ec9fc25dc6f883afac06ad31ff9c66482ac97
Artem Grinev <agrinev@manjaro.org>
2023-01-10 15:17:35 +0400

    [daemon] Add package application service

---
+++ daemon/core/application/services/PackageService.cpp
@@ -0,0 +1,18 @@
+/* === This file is part of bxt ===
+ *
+ *   SPDX-FileCopyrightText: 2022 Artem Grinev <agrinev@manjaro.org>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#include "PackageService.h"
+
+namespace bxt::Core::Application {
+
+coro::task<std::span<Package>>
+    PackageService::get_packages(const PackageSectionDTO& section_dto) const {
+    auto section = m_section_dto_mapper.map(section_dto);
+
+    return m_repository.find_by_section_async(section);
+}
+
+} // namespace bxt::Core::Application
---
+++ daemon/core/application/services/PackageService.h
@@ -0,0 +1,32 @@
+/* === This file is part of bxt ===
+ *
+ *   SPDX-FileCopyrightText: 2022 Artem Grinev <agrinev@manjaro.org>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#pragma once
+
+#include "core/application/dtos/PackageSectionDTO.h"
+#include "core/domain/repositories/PackageRepositoryBase.h"
+
+#include <coro/task.hpp>
+
+namespace bxt::Core::Application {
+
+class PackageService {
+public:
+    PackageService(Domain::PackageRepositoryBase& repository,
+                   Utilities::Mapper<Package::Section, PackageSectionDTO>&
+                       section_dto_mapper)
+        : m_repository(repository), m_section_dto_mapper(section_dto_mapper) {}
+
+    coro::task<std::span<Domain::Package>>
+        get_packages(const PackageSectionDTO& section_dto) const;
+
+private:
+    Domain::PackageRepositoryBase& m_repository;
+    Utilities::Mapper<Package::Section, PackageSectionDTO>&
+        m_section_dto_mapper;
+};
+
+} // namespace bxt::Core::Application
