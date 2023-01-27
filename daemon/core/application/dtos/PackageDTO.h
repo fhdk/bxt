1a50339f4cfce51ad786c3aacbabd5332fbd56f5
Artem Grinev <agrinev@manjaro.org>
2023-01-27 23:48:12 +0400

    [daemon] Add PackageDTO mappers

--- daemon/core/application/dtos/PackageDTO.h
+++ daemon/core/application/dtos/PackageDTO.h
@@ -7,6 +7,8 @@
 #pragma once
 
 #include "PackageSectionDTO.h"
+#include "core/domain/entities/Package.h"
+#include "utilities/Mapper.h"
 
 #include <filesystem>
 #include <string>
@@ -20,3 +22,26 @@ struct PackageDTO {
     auto operator<=>(const PackageDTO& other) const = default;
 };
 } // namespace bxt::Core::Application
+
+namespace {
+using namespace bxt::Core::Domain;
+using namespace bxt::Core::Application;
+} // namespace
+
+template<> struct bxt::Utilities::Mapper<PackageDTO, Package> {
+    PackageDTO map(const Package& from) const {
+        return Core::Application::PackageDTO {
+            .section = m_section_mapper.map(from.section()),
+            .name = from.name(),
+            .filepath = from.filepath()};
+    }
+    Mapper<PackageSectionDTO, Section> m_section_mapper;
+};
+
+template<> struct bxt::Utilities::Mapper<Package, PackageDTO> {
+    Package map(const PackageDTO& from) const {
+        auto section = m_section_mapper.map(from.section);
+        return Package::from_filepath(section, from.filepath);
+    }
+    Mapper<Section, PackageSectionDTO> m_section_mapper;
+};
