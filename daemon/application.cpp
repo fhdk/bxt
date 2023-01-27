598c22c79fc6c57edf6cffcb73957df10eb7e559
Artem Grinev <agrinev@manjaro.org>
2023-01-27 23:48:50 +0400

    [daemon] use using namespace directive for log setup

--- daemon/application.cpp
+++ daemon/application.cpp
@@ -8,15 +8,17 @@
 #include <boost/log/utility/setup.hpp>
 
 void setup_logger() {
-    boost::log::add_common_attributes();
-    boost::log::add_console_log(std::cout,
-                                boost::log::keywords::format =
-                                    "[%TimeStamp%][%Severity%]: %Message%",
-                                boost::log::keywords::auto_flush = true);
-    boost::log::add_file_log("journal.log",
-                             boost::log::keywords::format =
-                                 "[%TimeStamp%][%Severity%]: %Message%",
-                             boost::log::keywords::auto_flush = true);
+    using namespace boost::log;
+
+    add_common_attributes();
+    add_console_log(std::cout,
+                    boost::log::keywords::format =
+                        "[%TimeStamp%][%Severity%]: %Message%",
+                    boost::log::keywords::auto_flush = true);
+    add_file_log("journal.log",
+                 boost::log::keywords::format =
+                     "[%TimeStamp%][%Severity%]: %Message%",
+                 boost::log::keywords::auto_flush = true);
 }
 
 int main() {
