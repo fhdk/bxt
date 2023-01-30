11aaa23e10607588dc4e38f392769d0a02440665
Artem Grinev <agrinev@manjaro.org>
2023-01-30 17:45:35 +0400

    [daemon] Add DeploymentService base class

---
+++ daemon/core/application/services/DeploymentService.h
@@ -0,0 +1,22 @@
+/* === This file is part of bxt ===
+ *
+ *   SPDX-FileCopyrightText: 2022 Artem Grinev <agrinev@manjaro.org>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#pragma once
+
+#include "core/application/dtos/PackageDTO.h"
+
+#include <coro/task.hpp>
+
+namespace bxt::Core::Application {
+
+class DeploymentService {
+public:
+    virtual ~DeploymentService() = default;
+
+    virtual coro::task<void> deploy(PackageDTO package) = 0;
+};
+
+} // namespace bxt::Core::Application
