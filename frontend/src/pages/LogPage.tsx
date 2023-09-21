f5ebbbae5de7d9b4784f719e8ff24f538f789ec2
Artem Grinev <agrinev@manjaro.org>
2023-09-21 19:46:50 +0000

    [frontend] Sort logs by time

--- frontend/src/pages/LogPage.tsx
+++ frontend/src/pages/LogPage.tsx
@@ -2,6 +2,16 @@ import { Table } from "react-daisyui";
 import logs from "./logs.json";
 import { usePackageLogs } from "../hooks/BxtHooks";
 
+const dateCompare = (a: ILogEntry, b: ILogEntry): number => {
+  if (a.time < b.time) {
+    return -1;
+  }
+  if (a.time > b.time) {
+    return 1;
+  }
+  return 0;
+}
+
 export default (props: any) => {
 
   const entries = usePackageLogs();
@@ -17,16 +27,18 @@ export default (props: any) => {
         </Table.Head>
 
         <Table.Body>
-          {entries.map((value, index) => {
-            return (
-              <Table.Row>
-                <span>{index}</span>
-                <span>{value.id}</span>
-                <span>{value.package}</span>
-                <span>{value.time.toLocaleString()}</span>
-              </Table.Row>
-            );
-          })}
+          {entries
+            .sort((a, b) => -dateCompare(a, b))
+            .map((value, index) => {
+              return (
+                <Table.Row>
+                  <span>{index}</span>
+                  <span>{value.id}</span>
+                  <span>{value.package}</span>
+                  <span>{value.time.toLocaleString()}</span>
+                </Table.Row>
+              );
+            })}
         </Table.Body>
       </Table>
     </div>
