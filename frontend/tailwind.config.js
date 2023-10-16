812b9241f9252203ad48d6eb8fd3c05f85064232
Artem Grinev <agrinev@manjaro.org>
2023-10-16 04:10:17 +0000

    [frontend] Format tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,50 +1,25 @@
 module.exports = {
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-        
-
-
-"primary": "#35bfa4",
-        
-
-
-"secondary": "#3daee9",
-        
-
-
-"accent": "#60ffca",
-        
-
-
-"neutral": "#fcfcfc",
-        
-
-
-"base-100": "#eff0f1",
-        
-
-
-"info": "#799CE6",
-        
-
-
-"success": "#27ae60",
-        
-
-
-"warning": "#f67400",
-        
-
-
-"error": "#da4453",
-        },
-      },
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#35bfa4",
+                    secondary: "#3daee9",
+                    accent: "#60ffca",
+                    neutral: "#fcfcfc",
+                    "base-100": "#eff0f1",
+                    info: "#799CE6",
+                    success: "#27ae60",
+                    warning: "#f67400",
+                    error: "#da4453"
+                }
+            }
+        ]
+    },
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "node_modules/daisyui/dist/**/*.js",
+        "node_modules/react-daisyui/dist/**/*.js"
     ],
-  },
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
-    plugins: [require("@tailwindcss/typography"), require('daisyui')],
-  }
+    plugins: [require("@tailwindcss/typography"), require("daisyui")]
+};
