22e6c51a3b51d64b69291a101ef6bc626d6d52ab
Artem Grinev <agrinev@manjaro.org>
2024-03-02 20:26:08 +0000

    [daemon] Fix missing year in SPDX
    Seems class generator template substitution gone wrong and didn't fill in %YEAR% placeholders in some places

--- daemon/utilities/lmdb/BoostSerializer.h
+++ daemon/utilities/lmdb/BoostSerializer.h
@@ -1,6 +1,6 @@
 /* === This file is part of bxt ===
  *
- *   SPDX-FileCopyrightText: %YEAR% Artem Grinev <agrinev@manjaro.org>
+ *   SPDX-FileCopyrightText: 2023 Artem Grinev <agrinev@manjaro.org>
  *   SPDX-License-Identifier: GPL-3.0-or-later
  *
  */
