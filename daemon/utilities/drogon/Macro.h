b8a3261a3cafc9bf023230ff641f3556d672b571
Artem Grinev <agrinev@manjaro.org>
2024-03-02 20:58:54 +0000

    [daemon] Add permission check macro

--- daemon/utilities/drogon/Macro.h
+++ daemon/utilities/drogon/Macro.h
@@ -10,3 +10,18 @@
 
 #define BXT_JWT_ADD_METHOD_TO(...) \
     ADD_METHOD_TO(__VA_ARGS__, drogon::Options, "bxt::Presentation::JwtFilter")
+
+#define BXT_JWT_CHECK_PERMISSIONS(permissions, requestptr)                     \
+                                                                               \
+    if (!co_await m_permission_service.check(                                  \
+            permissions,                                                       \
+            requestptr->getAttributes()->get<std::string>("jwt_username"))) {  \
+        Json::Value result;                                                    \
+        result["error"] = "Not sufficient permissions to perform this action"; \
+        result["status"] = "error";                                            \
+                                                                               \
+        auto response = drogon::HttpResponse::newHttpJsonResponse(result);     \
+        response->setStatusCode(drogon::k401Unauthorized);                     \
+                                                                               \
+        co_return response;                                                    \
+    }
