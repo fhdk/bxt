49edb84c1f88bfd6e0da2e6bac18aaa1d055ecce
Artem Grinev <agrinev@manjaro.org>
2024-03-21 19:09:59 +0000

    style(frontend): Format frontend files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,14 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 import { Theme } from "react-daisyui";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Theme dataTheme='light'>
-      <App />
-    </Theme>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Theme dataTheme="light">
+            <App />
+        </Theme>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
