5ccc8255d55f7626d82712ab4dc787b07f19d9dd
Artem Grinev <agrinev@manjaro.org>
2024-07-13 15:52:33 +0000

    build: update ui path for swagger
    After #77 it uses `web` instead of `frontend`

--- daemon/swagger/configure_files.cmake
+++ daemon/swagger/configure_files.cmake
@@ -1,8 +1,8 @@
 configure_file(${LIST_DIR}/index.html.in
-                ${BINARY_DIR}/bin/frontend/swagger/index.html)
+                ${BINARY_DIR}/bin/web/swagger/index.html)
 
 configure_file(${LIST_DIR}/swagger-initializer.js.in
-                ${BINARY_DIR}/bin/frontend/swagger/swagger-initializer.js)
+                ${BINARY_DIR}/bin/web/swagger/swagger-initializer.js)
 
 configure_file(${LIST_DIR}/openapi.yml.in
-                ${BINARY_DIR}/bin/frontend/swagger/openapi.yml)
+                ${BINARY_DIR}/bin/web/swagger/openapi.yml)
