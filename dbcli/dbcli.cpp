32fa23dafdffb9ef522fdd1dccfa609032de4bf6
Artem Grinev <agrinev@manjaro.org>
2024-07-23 17:46:40 +0000

    feat(dbcli): add validation and rebuild
    Adds validation and rebuild logic to dbcli.
    
     - `validate` shows if there are some packages reference non-existing files;
     - `rebuild` extends `validate` by also recreating Descs in database based on file's PKGINFO

--- dbcli/dbcli.cpp
+++ dbcli/dbcli.cpp
@@ -6,16 +6,107 @@
  */
 #include "persistence/box/record/PackageRecord.h"
 #include "utilities/lmdb/CerealSerializer.h"
+#include "utilities/to_string.h"
 
 #include <coro/io_scheduler.hpp>
 #include <coro/sync_wait.hpp>
 #include <cstdio>
+#include <fmt/color.h>
 #include <fmt/core.h>
 #include <lmdbxx/lmdb++.h>
 #include <string>
 
 using Serializer = bxt::Utilities::LMDB::CerealSerializer<
     bxt::Persistence::Box::PackageRecord>;
+
+int validate_and_rebuild(lmdb::txn& transaction,
+                         lmdb::dbi& db,
+                         bool rebuild_descfile = false) {
+    auto cursor = lmdb::cursor::open(transaction, db);
+    std::string_view key, value;
+
+    int error_count = 0;
+
+    while (cursor.get(key, value, MDB_NEXT)) {
+        auto record = Serializer::deserialize(std::string(value));
+        if (!record) {
+            fmt::print(stderr, fg(fmt::terminal_color::red),
+                       "{}: Failed to deserialize record: {}\n",
+                       record->id.to_string(), record.error().what());
+            if (rebuild_descfile) { return 1; }
+            error_count++;
+            continue;
+        }
+
+        bool needs_update = false;
+
+        for (auto& [location, description] : record->descriptions) {
+            if (!std::filesystem::exists(description.filepath)) {
+                fmt::print(stderr, fg(fmt::terminal_color::red),
+                           "{} ({}): File not found: {}\n",
+                           record->id.to_string(), bxt::to_string(location),
+                           description.signature_path->string());
+                if (rebuild_descfile) { return 1; }
+                error_count++;
+                continue;
+            }
+
+            if (description.signature_path
+                && !std::filesystem::exists(*description.signature_path)) {
+                fmt::print(stderr, fg(fmt::terminal_color::red),
+                           "{} ({}): Signature file not found: {}\n",
+                           record->id.to_string(), bxt::to_string(location),
+                           description.signature_path->string());
+                if (rebuild_descfile) { return 1; }
+                error_count++;
+                continue;
+            }
+
+            auto desc_result = bxt::Utilities::AlpmDb::Desc::parse_package(
+                description.filepath);
+
+            if (!desc_result) {
+                fmt::print(stderr, fg(fmt::terminal_color::red),
+                           "{} ({}): Failed to parse desc-file for: {}\n",
+                           record->id.to_string(), bxt::to_string(location),
+                           description.filepath.string());
+                if (rebuild_descfile) { return 1; }
+                error_count++;
+                continue;
+            }
+            fmt::print(fg(fmt::terminal_color::green), "{} ({}): Valid\n",
+                       record->id.to_string(), bxt::to_string(location));
+
+            if (rebuild_descfile) {
+                fmt::print("{} ({}): Rebuilding desc-file for: {}\n",
+                           record->id.to_string(), bxt::to_string(location),
+                           record->id.to_string());
+                description.descfile = std::move(*desc_result);
+                needs_update = true;
+            }
+        }
+
+        if (needs_update) {
+            auto serialized = Serializer::serialize(*record);
+            if (!serialized) {
+                fmt::print(stderr, fg(fmt::terminal_color::red),
+                           "{}: Failed to serialize record: {}\n",
+                           record->id.to_string(), serialized.error().what());
+                return 1;
+            }
+
+            try {
+                db.put(transaction, key, *serialized);
+            } catch (const std::exception& e) {
+                fmt::print(stderr, fg(fmt::terminal_color::red),
+                           "{}: Failed to update record: {}\n",
+                           record->id.to_string(), e.what());
+                return 1;
+            }
+        }
+    }
+    return error_count;
+}
 
 int main(int argc, char** argv) {
     auto lmdbenv = lmdb::env::create();
@@ -99,6 +190,30 @@ int main(int argc, char** argv) {
         } else {
             fmt::print(stderr, "Failed to delete value or value not found.\n");
         }
+    } else if (command == "validate") {
+        if (argc != 2) {
+            fmt::print(stderr, "Usage: {} validate\n", argv[0]);
+            return 1;
+        }
+
+        auto error_count = validate_and_rebuild(transaction, db, false);
+
+        if (error_count == 0) {
+            fmt::print("No errors found.\n");
+        } else {
+            fmt::print("{} errors found.\n", error_count);
+        }
+
+    } else if (command == "rebuild") {
+        if (argc != 2) {
+            fmt::print(stderr, "Usage: {} rebuild\n", argv[0]);
+            return 1;
+        }
+        if (validate_and_rebuild(transaction, db, true) == 0) {
+            fmt::print("Successfully rebuilt all packages.\n");
+            transaction.commit();
+        }
+
     } else {
         fmt::print(stderr, "Unknown command: {}\n", command);
         return 1;
